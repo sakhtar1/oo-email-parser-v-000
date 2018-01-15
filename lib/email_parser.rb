1c238d9dd0582d0f980fc373482ba0c3919f992c
Sharmina Akhtar <sakht318@gmail.com>
2018-01-15 15:19:35 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,6 +13,7 @@ class EmailParser
   def parse(parse_emails)
     parse_email = []
     parse_email = @emails.split(/[,\s]/)
+    parse_email = parse_email.reject { |email| e == ""}
     parse_email = @emails.uniq!
   end
 
